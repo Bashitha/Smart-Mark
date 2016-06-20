86f11e5aed93286f2943c31ace4c44817cefd1ec
Leshan Bashitha Wijegunawardana <bashithawije@gmail.com>
2016-06-20 19:12:50 +0530

    first commit

---
+++ smartMark/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace smartMark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.butExport = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.butCourseSearch = new System.Windows.Forms.Button();
+            this.txtCourseCode = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.butStudentSearch = new System.Windows.Forms.Button();
+            this.txtRegNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(580, 353);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(572, 327);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.butExport);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(572, 327);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // butExport
+            // 
+            this.butExport.Location = new System.Drawing.Point(387, 282);
+            this.butExport.Name = "butExport";
+            this.butExport.Size = new System.Drawing.Size(174, 23);
+            this.butExport.TabIndex = 4;
+            this.butExport.Text = "Export as PDF";
+            this.butExport.UseVisualStyleBackColor = true;
+            this.butExport.Click += new System.EventHandler(this.butStudentEmail_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(191, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(381, 256);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(182, 318);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.butCourseSearch);
+            this.panel3.Controls.Add(this.txtCourseCode);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(3, 148);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(176, 167);
+            this.panel3.TabIndex = 1;
+            // 
+            // butCourseSearch
+            // 
+            this.butCourseSearch.Location = new System.Drawing.Point(36, 138);
+            this.butCourseSearch.Name = "butCourseSearch";
+            this.butCourseSearch.Size = new System.Drawing.Size(75, 23);
+            this.butCourseSearch.TabIndex = 4;
+            this.butCourseSearch.Text = "Search";
+            this.butCourseSearch.UseVisualStyleBackColor = true;
+            this.butCourseSearch.Click += new System.EventHandler(this.butCourseSearch_Click);
+            // 
+            // txtCourseCode
+            // 
+            this.txtCourseCode.Location = new System.Drawing.Point(89, 84);
+            this.txtCourseCode.Name = "txtCourseCode";
+            this.txtCourseCode.Size = new System.Drawing.Size(81, 20);
+            this.txtCourseCode.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Course_code :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Course";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.butStudentSearch);
+            this.panel2.Controls.Add(this.txtRegNo);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(176, 139);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // butStudentSearch
+            // 
+            this.butStudentSearch.Location = new System.Drawing.Point(49, 95);
+            this.butStudentSearch.Name = "butStudentSearch";
+            this.butStudentSearch.Size = new System.Drawing.Size(75, 23);
+            this.butStudentSearch.TabIndex = 3;
+            this.butStudentSearch.Text = "Search";
+            this.butStudentSearch.UseVisualStyleBackColor = true;
+            this.butStudentSearch.Click += new System.EventHandler(this.butStudentSearch_Click);
+            // 
+            // txtRegNo
+            // 
+            this.txtRegNo.Location = new System.Drawing.Point(71, 53);
+            this.txtRegNo.Name = "txtRegNo";
+            this.txtRegNo.Size = new System.Drawing.Size(98, 20);
+            this.txtRegNo.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Reg. No :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 366);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button butCourseSearch;
+        private System.Windows.Forms.TextBox txtCourseCode;
+        private System.Windows.Forms.Button butStudentSearch;
+        private System.Windows.Forms.TextBox txtRegNo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button butExport;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ smartMark/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+using GmailSend;
+
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace smartMark
+{
+    public partial class Form1 : Form
+    {
+        private static DataTable dbdataset = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butStudentSearch_Click(object sender, EventArgs e)
+        {
+            string connString = "Server = localhost ; Port = 3306 ; Database = smartMark ; Uid = root ; password = bashitha; ";
+            MySqlConnection conn = new MySqlConnection(connString);
+            MySqlCommand command = conn.CreateCommand();
+            command.CommandText = " SELECT course_code,SUM(lec_hours*attendance*-1) AS total_absent_lectures,total_lec_hours FROM mark_attendance NATURAL JOIN course WHERE reg_no = '"+txtRegNo.Text+"' AND attendance = -1 GROUP BY 1;";
+
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            MySqlDataAdapter sda = new MySqlDataAdapter();
+            sda.SelectCommand = command;
+            DataTable dbdataset = new DataTable();
+            sda.Fill(dbdataset);
+
+            DataColumn percentageColumn = new DataColumn("Absent percentage(%)", typeof(int));
+            percentageColumn.DefaultValue = 0;
+            dbdataset.Columns.Add(percentageColumn);
+          
+            for (int i = 0; i < dbdataset.Rows.Count; i++)
+            {
+               dbdataset.Rows[i][3]=(double.Parse( dbdataset.Rows[i][1].ToString())/double.Parse(dbdataset.Rows[i][2].ToString() ))*100;
+               
+
+            }
+
+            BindingSource bs = new BindingSource();
+
+            bs.DataSource = dbdataset;
+            dataGridView1.DataSource = bs;
+
+            sendingMail(command, dbdataset);
+           
+            conn.Close();
+        }
+
+        private void butCourseSearch_Click(object sender, EventArgs e)
+        {
+            string connString = "Server = localhost ; Port = 3306 ; Database = smartMark ; Uid = root ; password = bashitha; ";
+            MySqlConnection conn = new MySqlConnection(connString);
+            MySqlCommand command = conn.CreateCommand();
+            command.CommandText = " SELECT reg_no,SUM(lec_hours*attendance*-1) AS total_absent_lectures,total_lec_hours FROM mark_attendance NATURAL JOIN course WHERE course_code = '" + txtCourseCode.Text + "' AND attendance = -1 GROUP BY 1;";
+
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            MySqlDataAdapter sda = new MySqlDataAdapter();
+            sda.SelectCommand = command;
+            dbdataset = new DataTable();//table
+            sda.Fill(dbdataset);
+
+            DataColumn percentageColumn = new DataColumn("Absent percentage(%)", typeof(int));
+            percentageColumn.DefaultValue = 0;
+            dbdataset.Columns.Add(percentageColumn);
+
+            for (int i = 0; i < dbdataset.Rows.Count; i++)
+            {
+                dbdataset.Rows[i][3] = (double.Parse(dbdataset.Rows[i][1].ToString()) / double.Parse(dbdataset.Rows[i][2].ToString())) * 100;
+
+
+            }
+
+            
+            BindingSource bs = new BindingSource();
+
+            bs.DataSource = dbdataset;
+            dataGridView1.DataSource = bs;
+            
+            conn.Close();
+        }
+
+        private void butStudentEmail_Click(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        public void ExportToPdf(DataTable dt)
+        {
+            Document document = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream("c://sample.pdf", FileMode.Create));
+            document.Open();
+            iTextSharp.text.Font font5 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 5);
+
+            PdfPTable table = new PdfPTable(dt.Columns.Count);
+            
+            float[] widths = new float[] { 4f, 4f, 4f, 4f };
+
+            table.SetWidths(widths);
+
+            table.WidthPercentage = 100;
+            
+            
+            PdfPCell cell = new PdfPCell(new Phrase("Products"));
+
+            cell.Colspan = dt.Columns.Count;
+
+            foreach (DataColumn c in dt.Columns)
+            {
+
+                table.AddCell(new Phrase(c.ColumnName, font5));
+            }
+
+            foreach (DataRow r in dt.Rows)
+            {
+                if (dt.Rows.Count > 0)
+                {
+                    table.AddCell(new Phrase(r[0].ToString(), font5));
+                    table.AddCell(new Phrase(r[1].ToString(), font5));
+                    table.AddCell(new Phrase(r[2].ToString(), font5));
+                    table.AddCell(new Phrase(r[3].ToString(), font5));
+                }
+            } document.Add(table);
+            document.Close();
+        }
+        private  void sendingMail(MySqlCommand command, DataTable dbdataset)
+        {
+            gmail gmlsnd = new gmail();
+            gmlsnd.auth("bashithawije@gmail.com", "1amgenius");
+
+            gmlsnd.Subject = "Low Attendance";
+
+            gmlsnd.Priority = 1;
+
+            for (int i = 0; i < dbdataset.Rows.Count; i++)
+            {
+                if (int.Parse(dbdataset.Rows[i][3].ToString()) >= 15)
+                {
+                    //taking email of appropriate student from the database
+                    String email;
+                   
+                    command.CommandText = " SELECT email FROM student WHERE reg_no=" + txtRegNo.Text;
+
+                    
+                    MySqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    email = reader["email"].ToString();
+
+                    Console.Write(email);
+
+                    //sending email
+                    gmlsnd.To = email;
+                    gmlsnd.Message = "Your attendance for " + dbdataset.Rows[i][0].ToString() + " is nearing the minimum faculty requirement.Please pay your attention for that.";
+
+                    try
+                    {
+                        gmlsnd.send();
+                        MessageBox.Show("Your Mail is sent");
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                
+            }
+        }
+    }
+}
